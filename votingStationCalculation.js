f115e45748ed4aa309868aff61f6c46a9d4cc641
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-09-03 18:36:52 +0000

    added solution for the exercise  Voting Station Calculation

---
+++ votingStationCalculation.js
@@ -0,0 +1,17 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+let chooseStations = function (stations) {
+  let goodStations = [];
+  for (let location of stations) {
+    if (location[1] >= 20 && (location[2] === 'school' || location[2] === 'community centre')) {
+      goodStations.push(location[0]);
+    }
+  }
+  return goodStations;
+}
+
+console.log(chooseStations(stations));
