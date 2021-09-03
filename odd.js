59bb7dfed47553ccb3e291982c29fee74b03a923
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-09-03 17:39:25 +0000

    added the exercise: isOdd()

---
+++ odd.js
@@ -0,0 +1,8 @@
+let isOdd = function (num) {
+  return num % 2 !== 0;
+}
+
+// console.log('4 is odd: ' + isOdd(4));
+// console.log('3 is odd: ' + isOdd(3));
+// console.log('2 is odd: ' + isOdd(2));
+console.log('1 is odd: ' + isOdd(1));
