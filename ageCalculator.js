f470ddc6993e2382281d417fdd5c40a30c95acb5
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-09-06 04:45:40 +0000

    added the following exercises: ageCalculator.js, areaCalculators.js, howManyHundreds.js, xMarksTheSpot.js.

---
+++ ageCalculator.js
@@ -0,0 +1,5 @@
+const ageCalculator = (nom, yearOfBirth, currentYear) => `${nom} is ${currentYear - yearOfBirth} years old.`
+
+
+console.log(ageCalculator("Miranda", 1982, 2021));
+console.log(ageCalculator("Ferdinand", 1979, 2021));
