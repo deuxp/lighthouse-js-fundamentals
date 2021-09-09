d97772855494f7c01452a50767c3a2d752b5abec
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-09-09 14:15:11 +0000

    modified the packing-list.js file

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,11 @@
+// iterating over an array with `for` -> for loops start from 0
+
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+let i = 0
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
+
